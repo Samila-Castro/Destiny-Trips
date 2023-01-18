0b086b0e8b16273afac0473d39e00352d132cfda
Samila Vieira <samilav1997@gmail.com>
2023-01-18 16:15:33 -0300

    refator scroll da page

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const menuItems = document.querySelectorAll(".menu-header a");
 menuItems.forEach((item) => item.addEventListener("click", scrollToIdOnclick));
 
 function scrollToIdOnclick(event) {
+  event.preventDefault();
   const element = event.target;
   const id = element.getAttribute("href");
-  const section = document.querySelector(id);
+  const to = document.querySelector(id).offsetTop;
+
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
 }
 
 function handleMobileNavBar() {
